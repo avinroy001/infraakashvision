e3ae2580cf8578ce2d46ebe5108832f3ea589639
Avinash <avin.roy001@gmail.com>
2025-10-04 07:41:32 +0530

    minor

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
-import Footer from "./Footer";
+import Footer from "./footer";
 
 const Layout = () => {
   return (
@@ -15,7 +15,7 @@ const Layout = () => {
       </main>
 
       {/* Footer */}
-      <Footer />
+     <Footer/>
     </div>
   );
 };
